94d4ed346d7e9e696ec93c0d0e2bb1f98b3e8dd0
Rindra <hei.rindra.2@gmail.com>
2026-07-13 08:42:54 +0300

    feat: add persist creation timestamp for picture submission

--- src/main/java/com/my/company/model/PictureSubmission.java
+++ src/main/java/com/my/company/model/PictureSubmission.java
@@ -1,7 +1,8 @@
 package com.my.company.model;
 
+import java.time.Instant;
 import java.util.UUID;
 import lombok.Builder;
 
 @Builder
-public record PictureSubmission(UUID id, String fileName, String email) {}
+public record PictureSubmission(UUID id, String fileName, String email, Instant createdAt) {}
--- src/main/java/com/my/company/repository/model/JPictureSubmission.java
+++ src/main/java/com/my/company/repository/model/JPictureSubmission.java
@@ -1,8 +1,10 @@
 package com.my.company.repository.model;
 
 import jakarta.persistence.*;
+import java.time.Instant;
 import java.util.UUID;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -13,6 +15,8 @@ import lombok.*;
 @Table(name = "picture_submission")
 public class JPictureSubmission {
   @Id @GeneratedValue private UUID id;
+
+  @CreationTimestamp private Instant createdAt;
 
   @Column(nullable = false)
   private String fileName;
